aed65207c9f04d3829733770e788663e5bdad320
nac <chotpisit080916c@gmail.com>
2022-08-23 23:53:12 +0700

    add misunderstood

---
+++ data_structure/inside_interval.cpp
@@ -0,0 +1,38 @@
+// copied from P'Noppakorn'code because I misunderstood the problem LOL.
+// Now clear
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check_range(map<int, int>::iterator it, int x) {
+    cout << it->first << " HOI " << it->second << "\n";
+    return (x >= it->first && x <= it->second);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int m, n;
+    cin >> m >> n;
+    map<int, int> d;
+
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+
+        d[a] = b;
+    }
+
+    while (n--) {
+        int x;
+        cin >> x;
+        auto it = d.lower_bound(x);
+        if (it->first == x) {
+            cout << 1 << " ";
+        } else if (it == d.end() || it->first > x) {
+            it--;
+            cout << check_range(it, x) << " ";
+        }
+    }
+    cout << "\n";
+}
