d8f13fdf1fe7d5daf9d80b2777674f0b8d9526a1
nac <chotpisit080916c@gmail.com>
2022-09-08 19:38:52 +0700

    add async

---
+++ typescript/test.ts
@@ -0,0 +1,50 @@
+// const taskA = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("A");
+//     }, 1);
+//     setTimeout(() => {
+//         reject(new Error("Crash"));
+//     }, 2);
+//   });
+// const taskB = new Promise<string> ((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("B");
+//     }, 2000);
+//     });
+// const taskC = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("C");
+//     }, 1);
+//     });
+
+
+
+
+// async function run () {
+//     const outputA = taskA;
+//     const outputB = taskB;
+//     const outputC = taskC;
+//     console.log(outputA);
+//     console.log(outputB);
+//     console.log(outputC);
+// }
+
+// run();
+// console.log("HOI!");
+
+
+const thisHasErro = () => {
+    throw new Error("BRUH");
+  };
+  
+try {
+console.log("Start process...");
+thisHasErro();
+console.log("End process...");
+} catch (error) {
+if (error instanceof Error) {
+    console.log(error.message);
+}
+} finally {
+console.log("Finally");
+}
