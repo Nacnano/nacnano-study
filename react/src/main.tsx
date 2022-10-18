04342e6c42f73fffa213b57eeae92fdd9859f033
nac <chotpisit080916c@gmail.com>
2022-10-18 18:56:18 +0700

    complete Thinc. React sharing section Day 1

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -6,6 +6,21 @@ import classes from "./style.module.css"
 import Profile from './Profile'
 import { useState } from 'react'
 
+interface BigNumberProps {
+  number: number;
+}
+function BigNumber(props: BigNumberProps) {
+  const {number} = props
+  return <h1> {number}</h1>
+}
+
+interface ColorDisplayProps {
+  color: string;
+}
+function ColorDisplay(props: ColorDisplayProps) {
+  return <h1 style={{color: props.color}}> Color </h1>
+}
+
 function Mycomponent (){
   const name = "Nac";
   const className = "heading";
@@ -37,41 +52,51 @@ function Mycomponent (){
   const [isOpen, setIsOpen] = useState(false);
   const [color, setColor] = useState("red");
   const [counter, SetCounter ] = useState(0); 
+  const [colors, SetColors] = useState(["red", "green"]);
   function handleClick() {
     setIsOpen(function (prev){
       return !prev;
     });
     setColor(function (prev) {
-      if (prev=='blue') return 'red';
+      if (prev=='cyan') return 'red';
       return 'cyan';
     });
-    SetCounter(counter + 1);
+    SetCounter(function (prev) {
+      return prev + 1;
+    });
+    SetColors(['blue', 'purple']);
   }
 
   return (
-  <div> 
-    
-    <Profile name="Nac" age={18} isSmart />
-    {peopleElement}
-    <h1 className={className}> Hello </h1> 
-    <p className={classes.heading1}> 
-      {formatName(name)} 
-    </p>
+  <> 
+    <div style={{backgroundColor: "cyan"}}>
+      <Profile name="Nac" age={18} isSmart />
+      {peopleElement}
+      <h1 className={className}> Hello </h1> 
+      <p className={classes.heading1}> 
+        {formatName(name)} 
+      </p>
+    </div>
 
-    <h3>{isOpen ? "Open" : "Disabled"}</h3>
-    <button onClick={handleClick} style={{backgroundColor: color}}> 
-      Press Me
-    </button>
-    <h3> {counter} Pressed </h3>
-  </div>);
+    <div style={{backgroundColor: "pink"}}>
+      <h3>{isOpen ? "Open" : "Disabled"}</h3>
+      <button onClick={handleClick} style={{backgroundColor: color}}> 
+        Press Me
+      </button>
+      <h3> {counter} Pressed </h3>
+      <BigNumber number={counter}/>
+    </div>
+
+    <div style={{backgroundColor: "orange"}}>
+      {colors.map(function(color) {
+        return <ColorDisplay color={color}/>
+      })}
+    </div>
+  </>);
 }
-
-
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Mycomponent/>
     <Mycomponent/>
     <App />
   </React.StrictMode>
