bc0cd8628b09105e3c80765800e4cd4b801372cd
nac <chotpisit080916c@gmail.com>
2023-02-06 00:52:14 +0700

    add next

--- graphql-nestjs/src/app.module.ts
+++ graphql-nestjs/src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({ driver: ApolloDriver }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
