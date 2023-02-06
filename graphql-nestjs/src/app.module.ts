3ab757506af1685210d035426f2d745161e42617
nac <chotpisit080916c@gmail.com>
2023-02-06 17:05:24 +0700

    add relation

--- graphql-nestjs/src/app.module.ts
+++ graphql-nestjs/src/app.module.ts
@@ -4,13 +4,25 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { PrismaModule } from './prisma/prisma.module';
+import { DogModule } from './dog/dog.module';
+import { OwnerModule } from './owner/owner.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      // plugins: [DEPRECATED],
+      typePaths: ['*/**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/types/graphql.ts'),
+        outputAs: 'class',
+      },
     }),
+    PrismaModule,
+    DogModule,
+    OwnerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
